8ccfb59d0d2a38153ad24a5097ee8792fec9b181
Ralf Westphal <info@ralfw.de>
2014-03-30 10:27:56 +0200

    Red: acceptance tests with popular texts
    No class, no method

---
+++ TDDaiymi/KataWordWrap/AcceptanceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataWordWrap
+{
+    [TestFixture]
+    public class AcceptanceTests
+    {
+        [TestCase("The quick brown fox jumps over the lazy dog", 9, "The quick\nbrown fox\njumps\nover the\nlazy dog")]
+        [TestCase("Mary had a little lamb", 5, "Mary\nhad a\nlittl\ne\nlamb")]
+        public void Popular_texts(string text, int maxLineLength, string expected)
+        {
+            var sut = new Wrapper();
+            var result = sut.Wrap(text, maxLineLength);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
