4ae8d3ee82fd3b87282b02451089e8e6593243b3
Ralf Westphal <info@ralfw.de>
2014-03-30 10:39:34 +0200

    Red: second scaffolding test - wrap two short words

--- TDDaiymi/KataWordWrap/ScaffoldingTests.cs
+++ TDDaiymi/KataWordWrap/ScaffoldingTests.cs
@@ -6,6 +6,16 @@ namespace KataWordWrap
     public class ScaffoldingTests
     {
         [Test]
+        public void Wrap_two_short_words()
+        {
+            var sut = new Wrapper();
+
+            var result = sut.Wrap("word1 word2", 5);
+
+            Assert.AreEqual("word1\nword2", result);
+        }
+
+        [Test]
         public void Single_short_word_in_line()
         {
             var sut = new Wrapper();
@@ -13,6 +23,6 @@ namespace KataWordWrap
             var result = sut.Wrap("word", 4);
 
             Assert.AreEqual("word", result);
-        } 
+        }
     }
 }
