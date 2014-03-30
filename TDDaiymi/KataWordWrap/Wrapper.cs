9f9a13b6a1a560a9ca1785740ea054d72a51afcf
Ralf Westphal <info@ralfw.de>
2014-03-30 11:34:24 +0200

    Red: translated solution idea into methods

--- TDDaiymi/KataWordWrap/Wrapper.cs
+++ TDDaiymi/KataWordWrap/Wrapper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace KataWordWrap
 {
@@ -6,12 +7,19 @@ namespace KataWordWrap
     {
         public string Wrap(string text, int maxLineLength)
         {
-            /*
-             * Solution design to wrap short words (s. flow 1 and 2):
-             * 1. Split line into words
-             * 2. Build lines from words
-             */
-            return text;
+            var words = Split_line_into_words(text);
+            return Build_lines_from_words(words, maxLineLength);
+        }
+
+
+        private IEnumerable<string> Split_line_into_words(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string Build_lines_from_words(IEnumerable<string> words, int maxLineLength)
+        {
+            throw new NotImplementedException();
         }
     }
 }
