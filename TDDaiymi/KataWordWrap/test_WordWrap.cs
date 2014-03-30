9dd3be9fb3f7ba48552377c81fcb63598a643487
Ralf Westphal <info@ralfw.de>
2014-03-30 10:10:21 +0200

    Added NUnit and NCrunch and JustMock

--- TDDaiymi/KataWordWrap/test_WordWrap.cs
+++ TDDaiymi/KataWordWrap/test_WordWrap.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace KataWordWrap
 {
-    public class Class1
+    [TestFixture]
+    public class test_WordWrap
     {
     }
 }
