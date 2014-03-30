7f68fba5cbb8768437fe4085aa061b6cb1fcdb24
Ralf Westphal <info@ralfw.de>
2014-03-30 10:35:21 +0200

    Red: first scaffolding test - single short word in line

---
+++ TDDaiymi/KataWordWrap/ScaffoldingTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace KataWordWrap
+{
+    [TestFixture]
+    public class ScaffoldingTests
+    {
+        [Test]
+        public void Single_short_word_in_line()
+        {
+            var sut = new Wrapper();
+
+            var result = sut.Wrap("word", 4);
+
+            Assert.AreEqual("word", result);
+        } 
+    }
+}
